238263f5ed66a8b526e3f1b9f75762c2bfd21f91
Daniel Lwetabe <acehanks@gmail.com>
2020-04-02 00:23:07 +0300

    implement get lastName functionality and test - create failing test for get full name

--- app/Models/user.php
+++ app/Models/user.php
@@ -4,12 +4,21 @@ namespace App\Models;
 
 class User {
     public $first_name;
+    public $last_name;
 
     public function setFirstName($firstName) {
         $this->first_name = $firstName;
     }
 
     public function getFirstName() {
-        return 'Billy';
+        return $this->first_name;
+    }
+
+    public function setLastName($lastName) {
+        $this->last_name = $lastName;
+    }
+
+    public function getLastName() {
+        return $this->last_name;
     }
 }
--- tests/unit/userTest.php
+++ tests/unit/userTest.php
@@ -11,4 +11,17 @@ class UserTest extends TestCase {
         $this->assertEquals($user->getFirstName(), 'Billy');
     }
 
+    public function testGetLastName() {
+        
+        $user = new \App\Models\User;
+        $user->setLastName('Armstring');
+        $this->assertEquals($user->getLastName(), 'Armstring');
+    }
+
+    public function testFullNameIsReturned() {
+        $user = new \App\Models\User;
+        $user->setFirstName('Billy');
+        $user->setLastName('Armstring');
+        $this->assertEquals($user->getFullName(), 'Billy Armstring');
+    }
 }
