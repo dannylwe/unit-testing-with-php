3ab33fe26ee18aba15ac07f7273822714d4fec63
Daniel Lwetabe <acehanks@gmail.com>
2020-04-01 23:52:31 +0300

    simple test example

--- tests/unit/sampleTest.php
+++ tests/unit/sampleTest.php
@@ -4,4 +4,12 @@ use PHPUnit\Framework\TestCase;
 
 class SampleTest extends TestCase {
 
+    public function testTrueAssertsToTrue() {
+        $this->assertTrue(true);
+    }
+
+    public function testSumAddsToTwo() {
+        $this->assertEquals(1+1, 2);
+    }
+
 }
